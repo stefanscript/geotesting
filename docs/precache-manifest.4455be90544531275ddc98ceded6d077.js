1c6249279d8d4e063036cd8f49a22d273a8856eb
stefan <stefan.anton@mkodo.com>
2019-04-09 10:05:57 +0100

    refactor fix current position test to fail if all no jitter

---
+++ docs/precache-manifest.4455be90544531275ddc98ceded6d077.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5333f60c462f64b09256",
+    "url": "/geotesting/static/js/runtime~main.22f17e73.js"
+  },
+  {
+    "revision": "168c27ef6ceabd269d6b",
+    "url": "/geotesting/static/js/main.a70cc065.chunk.js"
+  },
+  {
+    "revision": "683fa498e10e3c2b2f19",
+    "url": "/geotesting/static/js/2.d9e169f2.chunk.js"
+  },
+  {
+    "revision": "168c27ef6ceabd269d6b",
+    "url": "/geotesting/static/css/main.e9dc094c.chunk.css"
+  },
+  {
+    "revision": "d92bba7da9a8d0e8f37405e833f552d6",
+    "url": "/geotesting/index.html"
+  }
+];
