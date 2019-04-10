90192c18429bbdc1120b25fedae24173ef76f088
stefan <stefan.anton@mkodo.com>
2019-04-10 12:17:04 +0100

    try fix routes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./Home";
 
 export default function App() {
     return (
-        <Router basename="/geotesting">
+        <Router>
             <header><h2>Geo Testing</h2></header>
             <Switch>
                 <Route exact path="/" component={Home} />
