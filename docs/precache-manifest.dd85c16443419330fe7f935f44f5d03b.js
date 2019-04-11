8b1336d33181e318c45d444e02c98a1792ee9762
stefan <stefan.anton@mkodo.com>
2019-04-11 10:52:31 +0100

    v no

--- docs/precache-manifest.dd85c16443419330fe7f935f44f5d03b.js
+++ docs/precache-manifest.dd85c16443419330fe7f935f44f5d03b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/geotesting/static/js/runtime~main.22f17e73.js"
   },
   {
-    "revision": "27333334410bf76c2538",
-    "url": "/geotesting/static/js/main.b0c1c742.chunk.js"
+    "revision": "6ca28926de41ca02e2d3",
+    "url": "/geotesting/static/js/main.1c462f15.chunk.js"
   },
   {
     "revision": "1d33c14871a0d797a7dd",
     "url": "/geotesting/static/js/2.e54ff91d.chunk.js"
   },
   {
-    "revision": "27333334410bf76c2538",
+    "revision": "6ca28926de41ca02e2d3",
     "url": "/geotesting/static/css/main.1438e6ca.chunk.css"
   },
   {
-    "revision": "24b8247927137b612b097597212a59be",
+    "revision": "d6793a5d4df5b6e87648dd4a36eaa924",
     "url": "/geotesting/index.html"
   }
 ];
