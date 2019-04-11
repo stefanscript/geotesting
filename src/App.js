796f615cc04415f7f29df06a79ae49c46fab6073
stefan <stefan.anton@mkodo.com>
2019-04-11 10:37:07 +0100

    v no

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import TestingGround from "./TestingGround";
 import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
 import Home from "./Home";
 
+import packageJson from '../package.json';
+const vNo = packageJson.version;
+
+
 export default function App() {
     return (
         <Router>
-            <header><h2>Geo Testing</h2></header>
+            <header><h2>Geo Testing {`v` + vNo}</h2></header>
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route component={TestingGround} />
