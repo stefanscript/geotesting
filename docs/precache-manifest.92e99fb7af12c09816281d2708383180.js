934c6f515cb2259e58821993fb486134c0cfcf47
stefan <stefan.anton@mkodo.com>
2019-04-11 11:23:52 +0100

    change to hashrouter

---
+++ docs/precache-manifest.92e99fb7af12c09816281d2708383180.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5333f60c462f64b09256",
+    "url": "/geotesting/static/js/runtime~main.22f17e73.js"
+  },
+  {
+    "revision": "f4f08369d78835cf01e8",
+    "url": "/geotesting/static/js/main.453191aa.chunk.js"
+  },
+  {
+    "revision": "1fe27f33b47e3cd6e2a6",
+    "url": "/geotesting/static/js/2.a09c212c.chunk.js"
+  },
+  {
+    "revision": "f4f08369d78835cf01e8",
+    "url": "/geotesting/static/css/main.1438e6ca.chunk.css"
+  },
+  {
+    "revision": "5ec71a659109f69304ea4bd91e602ea8",
+    "url": "/geotesting/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/geotesting/precache-manifest.0415b23f2c712d9ba1a2d9be4b756432.js"
+  "/geotesting/precache-manifest.92e99fb7af12c09816281d2708383180.js"
 );
 
 workbox.clientsClaim();
