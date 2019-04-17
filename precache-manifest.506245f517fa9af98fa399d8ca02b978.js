53576286c6466bc160bc13f8811413ab668b0f65
stefan <stefan.anton@mkodo.com>
2019-04-17 13:40:18 +0100

    Updates

--- precache-manifest.506245f517fa9af98fa399d8ca02b978.js
+++ precache-manifest.506245f517fa9af98fa399d8ca02b978.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/geotesting/static/js/runtime~main.22f17e73.js"
   },
   {
-    "revision": "590f1c448102d1b146d3",
-    "url": "/geotesting/static/js/main.9c79c3b4.chunk.js"
+    "revision": "c4a8496ffb97f48b3661",
+    "url": "/geotesting/static/js/main.ab4e4177.chunk.js"
   },
   {
     "revision": "1fe27f33b47e3cd6e2a6",
     "url": "/geotesting/static/js/2.a09c212c.chunk.js"
   },
   {
-    "revision": "590f1c448102d1b146d3",
+    "revision": "c4a8496ffb97f48b3661",
     "url": "/geotesting/static/css/main.1438e6ca.chunk.css"
   },
   {
-    "revision": "a9ee36a6dca92cfa6b4589f59bafea61",
+    "revision": "618e938a093343a5cf49a44281953115",
     "url": "/geotesting/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/geotesting/precache-manifest.10989a533b55d89a12182128b8faaac6.js"
+  "/geotesting/precache-manifest.506245f517fa9af98fa399d8ca02b978.js"
 );
 
 workbox.clientsClaim();
