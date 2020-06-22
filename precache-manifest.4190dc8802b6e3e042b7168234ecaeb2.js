0d041fa5a078cccabb1beaad2caeac8f6958c70d
stefanscript <sha32768@gmail.com>
2020-06-22 09:38:34 +0100

    Updates

---
+++ precache-manifest.4190dc8802b6e3e042b7168234ecaeb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808e5e70aa71dbdf0c82eebfa4a955f9",
+    "url": "/geotesting/index.html"
+  },
+  {
+    "revision": "8bec4aa476e4f0e12485",
+    "url": "/geotesting/static/css/main.4e95f68b.chunk.css"
+  },
+  {
+    "revision": "a52d2d7327a5081de135",
+    "url": "/geotesting/static/js/2.b0aced1d.chunk.js"
+  },
+  {
+    "revision": "e814e956e87b64060b0351e69977a48e",
+    "url": "/geotesting/static/js/2.b0aced1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bec4aa476e4f0e12485",
+    "url": "/geotesting/static/js/main.bed18ce1.chunk.js"
+  },
+  {
+    "revision": "7aef79acc4bf88e755c3",
+    "url": "/geotesting/static/js/runtime-main.63d5e7dc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/geotesting/precache-manifest.0c68404fe35be3e077f7a45172eed231.js"
+  "/geotesting/precache-manifest.4190dc8802b6e3e042b7168234ecaeb2.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/geotesting/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/geotesting/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
